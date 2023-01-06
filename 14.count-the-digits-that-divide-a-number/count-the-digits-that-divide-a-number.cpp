85d565e589dc6d0485d01a93a186ae256c5b9e44
Ashab-Asir <ashabasir101@gmail.com>
2023-01-06 21:03:49 +0600

    count-the-digits-that-divide-a-number code added

---
+++ 14.count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.cpp
@@ -0,0 +1,16 @@
+//Problem Link:
+class Solution {
+public:
+    int countDigits(int num) {
+        int copynum=num;
+        int count=0;
+        while(num!=0){
+            int digit=num%10;
+            if(copynum%digit==0){
+                count++;
+            }
+            num=num/10;
+        }
+        return count;
+    }
+};
